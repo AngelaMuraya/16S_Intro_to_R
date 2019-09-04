37b0abd99e57f3e3dc7a2feb3d87d87f32f81a62
AngelaMuraya <angelimuraya@gmail.com>
2019-09-04 16:43:25 +0300

    summary of steps used to create and edit repo and project

--- testfolder/testfolder.R
+++ testfolder/testfolder.R
@@ -1,5 +1,18 @@
-# testfolder
-
-# retrying git commit through rstudio
-
-# Redoing git commit a second time
+# # testfolder
+# 
+# # retrying git commit through rstudio
+# 
+# # Redoing git commit a second time
+# 
+# Setting up a version control repo for R project directly through R Studio is the best option.
+# 
+# 1. Create a repo from github. 
+# If you add a READ.ME, you will need to pull the repo from origin master. Best practice is to add the READ>ME later after first commit.
+# 2. Create a project directory and initialize with git version control
+# 3. Make changes to the project directory; commit the changes through three step:
+#   a. Stage the new changes
+#   b. click commit and add message
+#   c. click push
+# 4. Confirm changes in github repo
+# 5. Create READ.ME, if applicable.
+# 6. Git pull through R Studio (Git > Pull Branch)
